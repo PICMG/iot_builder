3364a03119d1a4bae14496db53b6cdda69eeb3ec
dsandy12 <dlsandy@asu.edu>
2026-01-30 08:43:51 -0700

    Add cmake build process.  Fix build issues and bugs.

--- src/builder/platform_Adapter.h
+++ src/builder/platform_Adapter.h
@@ -1,8 +1,17 @@
 #pragma once
 
-#define TYPE_SINT8				signed char
-#define TYPE_UINT8				unsigned char
-#define TYPE_SINT16				signed int
-#define TYPE_UINT16				unsigned int
-#define TYPE_SINT32				signed long
-#define TYPE_UINT32				unsigned long
+// Indicate that this platform supports the standard fixed-width integer types.
+// This allows upstream headers (stdTypes.h) to include the appropriate header
+// and make use of int32_t, uint64_t, etc.
+#ifndef TYPES_SUPPORTS_SIZED_INTEGERS
+#define TYPES_SUPPORTS_SIZED_INTEGERS 1
+#endif
+
+#include <cstdint>
+
+#define TYPE_SINT8                signed char
+#define TYPE_UINT8                unsigned char
+#define TYPE_SINT16               signed int
+#define TYPE_UINT16               unsigned int
+#define TYPE_SINT32               signed long
+#define TYPE_UINT32               unsigned long
